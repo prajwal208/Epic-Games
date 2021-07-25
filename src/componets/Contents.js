2d4cdb3a7e12d6d5937ff101348ad5f3ee827549
Pr@jwal <76072862+prajwal208@users.noreply.github.com>
2021-07-25 21:00:34 +0530

    Update Contents.js

--- src/componets/Contents.js
+++ src/componets/Contents.js
@@ -1,52 +1,5 @@
 import React from 'react'
 import "./style1.css";
-import image from "./images/1.jpg";
-import image1 from "./images/2.jpg";
-import image2 from "./images/4.jpg";
-import image3 from "./images/3.jpg";
-import image4 from "./images/5.jpg";
-import image5 from "./images/8.jpg";
-import image6 from "./images/7.jpg";
-import image7 from "./images/9.jpg";
-import image8 from "./images/10.jpg";
-import image9 from "./images/11.jpg";
-import image10 from "./images/12.jpg";
-import image11 from "./images/13.jpg";
-import image12 from "./images/14.jpg";
-import image13 from "./images/15.jpg";
-import image14 from "./images/16.jpg";
-import image15 from "./images/17.jpg";
-import image16 from "./images/18.jpg";
-import image17 from "./images/19.jpg";
-import image20 from "./images/20.png";
-import image21 from "./images/21.jpg";
-import image22 from "./images/22.png";
-import image23 from "./images/23.jpg";
-import image24 from "./images/24.jpg";
-import image25 from "./images/25.png";
-import image26 from "./images/26.jpg";
-import image27 from "./images/27.png";
-import image28 from "./images/28.jpg";
-import image29 from "./images/29.jpg";
-import image30 from "./images/30.jpeg";
-import image31 from "./images/31.jpg";
-import image100 from "./images/101.jpg";
-import image34 from "./images/34.png";
-import image35 from "./images/35.png";
-import image36 from "./images/36.jpeg";
-import image37 from "./images/37.jpeg";
-import image38 from "./images/38.jpg";
-import image39 from "./images/39.jpg";
-import image40 from "./images/40.jpg";
-import image41 from "./images/41.jpg";
-import image42 from "./images/42.jpg";
-
-
-
-
-
-
-
 
 
 
